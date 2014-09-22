0f636cf2b0d2efafbafe2dadb6393135ea3df4d8
Dale Salter <djsalter93@hotmail.com>
2014-09-22 22:43:29 +1000

    Added more tests

--- src/test/java/TestJunit.java
+++ src/test/java/TestJunit.java
@@ -7,6 +7,24 @@ public class TestJunit {
     public void testAdd() {
 
         System.out.println("Inside testAdd()");
+        String str= "Junit is working fine";
+
+        assertEquals("Junit is working fine",str);
+    }
+
+    @Test
+    public void trueTest() {
+
+        System.out.println("Inside anotherTest()");
+
+
+        assertEquals(true, true);
+    }
+
+    @Test
+    public void failTest() {
+
+        System.out.println("Inside failTest()");
 
         String str= "Junit is working fine";
 
