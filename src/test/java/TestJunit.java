a655297e9dbcb771a1db69c65996f7d918aba78a
Dale Salter <djsalter93@hotmail.com>
2014-09-24 21:18:29 +1000

    Both tests should pass

--- src/test/java/TestJunit.java
+++ src/test/java/TestJunit.java
@@ -1,37 +1,11 @@
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import junit.framework.TestCase;
 
 
-public class TestJunit {
+public class TestJunit extends TestCase {
     @Test
     public void testAdd() {
-        System.out.println("Inside testAddz()");
-
-        assertEquals(true,true);
-        assertEquals(true,true);
-        assertEquals(true,true);
-    }
-
-    @Test
-    public void trueTest() {
-        System.out.println("Inside anotherTest()");
-
-
-        assertEquals(true, true);
-    }
-
-    @Test
-    public void failTest() {
-        System.out.println("Inside failTest()");
-
-
-        assertEquals(true, true);
-    }
-
-    @Test
-    public void failTest2() {
-        System.out.println("Inside fail2Tests()");
-
-        assertEquals(true, true);
+        assertTrue("This is always true", true);
     }
 }
