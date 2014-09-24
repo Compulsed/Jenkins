f4a3ddfcb276da8c687f6c985b1180892771e9f5
Dale Salter <djsalter93@hotmail.com>
2014-09-24 18:52:27 +1000

    ADDED: Fail sort case

--- src/test/java/Main.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by dale on 22/09/2014.
- */
-public class Main {
-}
---
+++ src/test/java/TestSets.java
@@ -0,0 +1,46 @@
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by dale on 24/09/14.
+ */
+public class TestSets extends TestCase{
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testEmptyList() throws Exception {
+        List<Integer> aList = new ArrayList();
+
+        aList.add(5);
+        aList.add(10);
+        aList.add(20);
+        aList.add(3);
+
+        // Collections.sort(aList);
+
+        boolean isSorted = true;
+        try {
+            Integer number = aList.get(0);
+            for (Integer listItem : aList) {
+                if (listItem <= number) number = listItem;
+                else {
+                    isSorted = false;
+                    break;
+                }
+            }
+        } catch (Exception e){} // if there is nothing in the list
+
+        assertTrue("The list should be sorted", isSorted);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
